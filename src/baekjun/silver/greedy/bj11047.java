b2d9d740c2edb48f6b56c8a90840a60fbfd9705d
Yoon Jun Pil <galaxy160102@gmail.com>
2023-09-24 16:35:48 +0900

    그리디 / 백준 11047 / 최소 동전

---
+++ src/baekjun/silver/greedy/bj11047.java
@@ -0,0 +1,45 @@
+package baekjun.silver.greedy;
+
+import java.util.Scanner;
+
+public class bj11047 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int sum = scanner.nextInt();
+        int[] coins = new int[num];
+        for (int i = 0; i < num; i++) {
+            coins[i] = scanner.nextInt();
+        }
+
+        int cnt = 0;
+        int money = sum;
+        while(money != 0){
+            for (int i = num-1; i >= 0; i--) {
+                if(money - coins[i] >= 0){
+                    while(money - coins[i] >= 0){
+                        money -= coins[i];
+                        cnt++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(cnt);
+
+
+    }
+}
+//        문제
+//        준규가 가지고 있는 동전은 총 N종류이고, 각각의 동전을 매우 많이 가지고 있다.
+//
+//        동전을 적절히 사용해서 그 가치의 합을 K로 만들려고 한다. 이때 필요한 동전 개수의 최솟값을 구하는 프로그램을 작성하시오.
+//
+//        입력
+//        첫째 줄에 N과 K가 주어진다. (1 ≤ N ≤ 10, 1 ≤ K ≤ 100,000,000)
+//
+//        둘째 줄부터 N개의 줄에 동전의 가치 Ai가 오름차순으로 주어진다. (1 ≤ Ai ≤ 1,000,000, A1 = 1, i ≥ 2인 경우에 Ai는 Ai-1의 배수)
+//
+//        출력
+//        첫째 줄에 K원을 만드는데 필요한 동전 개수의 최솟값을 출력한다.
